b72e9ee9a0049f625a240fe4ce42daaafedf123f
Jing Flint <jingflint@gmail.com>
2020-05-07 13:38:22 -0700

    add deque

---
+++ circularBuffer.py
@@ -0,0 +1,7 @@
+'''
+circular buffer == circular queue == cyclic buffer = ring buffer
+
+cicular buffer is well-suited as a FIFO buffer. E.g. queue
+
+standard, non-circular buffer is well suited as a LIFO buffer
+'''
---
+++ pythonDeque.py
@@ -0,0 +1,18 @@
+'''
+Deque = Doubly Ended Queue
+collections.deque
+O(1) preferred over list O(n)
+
+'''
+
+append()
+appendleft()
+pop()
+popleft()
+
+# E.g.
+de = collections.deque([1, 2, 3])
+de.append(4)
+de.appendleft(6)
+de.pop()
+de.popLeft()
