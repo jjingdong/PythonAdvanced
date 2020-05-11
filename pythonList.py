433dcd8b0973e2fc29981f26592981d792d3adcb
Jing Flint <jingflint@gmail.com>
2020-05-11 01:06:10 -0700

    eidt

--- pythonList.py
+++ pythonList.py
@@ -1,3 +1,13 @@
 '''
 Python's list objects are really variable-length arrays, not Lisp-style linked lists
 '''
+
+lst = []
+lst.append(1)
+lst1 = [2]
+lst.extend(lst1)
+lst2 = [3]
+lst.append(lst2)
+lst = []
+lst.append(lst2)
+print(lst)
