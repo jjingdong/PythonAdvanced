a38b245510ce07d294d4ab09f5dc322348b283b0
Jing Flint <jingflint@gmail.com>
2020-05-27 23:43:05 -0700

    done bine

---
+++ pythonBin.py
@@ -0,0 +1,14 @@
+'''
+bin() convert an integer to binary
+'''
+
+#a = ob1
+a = bin(8)
+#b = 1
+# remove 'ob' prefix
+b = a[2:]
+#ob1000
+bin(8)
+#ob1001
+bin(9)
+
---
+++ pythonPermutationCombination.py
@@ -0,0 +1,37 @@
+'''
+permutation
+'''
+import itertools
+
+a = itertools.permutations([1,2,3])
+# a = permuations object
+# (1, 2, 3)
+# (1, 3, 2)
+# (2, 1, 3)
+# (2, 3, 1)
+# (3, 1, 2)
+# (3, 2, 1)
+for i in list(a):
+    print(i)
+
+# length = 2
+b = itertools.permutations([1,2,3], 2)
+# (1, 2)
+# (1, 3)
+# (2, 1)
+# (2, 3)
+# (3, 1)
+# (3, 2)
+for i in list(b):
+    print(i)
+
+'''
+combinations
+'''
+# combinations require 2nd arguments length
+c = itertools.combinations([1,2,3],2)
+# (1, 2)
+# (1, 3)
+# (2, 3)
+for i in list(c):
+    print(i)
