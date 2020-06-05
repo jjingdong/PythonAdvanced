cff26282791305646582901961574e58fc8a4680
Jing Flint <jingflint@gmail.com>
2020-06-04 19:38:54 -0700

    add zen

---
+++ zenOfPython.py
@@ -0,0 +1,21 @@
+'''
+Beautiful is better than ugly
+explicit is better than implicit
+simple is better than complex
+complex is better than complicated
+flat is better than nested
+sparse is better than dense
+readability counts
+special cases aren't special enought to break the rules
+although practicality beats purity
+errors should never pass silently
+unless explicityly silenced
+in the face of ambiguity, refuse the temptation to guess
+there should be one -- and preferably only one -- obvious way to do it
+although that way maybe be obvious at irst unless you're Dutch
+Now is better than never
+although never is often better than *right* now
+if the implementatation is hard to explain, it's a bad idea
+if the implementation is easy to explain, it maybe a good idea
+numspaces are one honking great idea -- let's do more of those!
+'''
