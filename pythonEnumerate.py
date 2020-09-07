72783931b6c9cf713b3e1fa6f70958863e18b5e1
Jing Flint <jingflint@gmail.com>
2020-09-06 19:38:28 -0700

    add enumerate

---
+++ pythonEnumerate.py
@@ -0,0 +1,11 @@
+'''
+enumerate(iterable, start=0)
+'''
+
+lst = ['abc', 'def', 'hij']
+# a = [(0, 'abc'), (1, 'def'), (2, 'hij')]
+a = list(enumerate(lst))
+
+str = 'abc'
+# a = [(2, 'a'), (3, 'b'), (4, 'c')]
+a = list(enumerate(strm 2))
