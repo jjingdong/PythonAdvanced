359d8949aaf768593d25cda9d028f27bcc1ee103
Jing Flint <jingflint@gmail.com>
2021-06-17 21:50:23 -0700

    random edit

--- pythonRandom.py
+++ pythonRandom.py
@@ -1,8 +1,12 @@
 '''
 random.ranrange(beg,end,step)
+random.randbytes(n), generate n random bytes
+random.randint(a,b), a <= num <= b
+random.shuffle(lst),  shuffle a list of objects
 '''
 import random
 
+# return 1 value in the range
 random.choice([1,3,5,7,9])
 
 random.randrange(10,20,2)
