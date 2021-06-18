16b84e40514163a6e9aded86e8f03b8d34b21d52
Jing Flint <jingflint@gmail.com>
2021-06-17 21:50:05 -0700

    heapq exit

--- pythonHeapq.py
+++ pythonHeapq.py
@@ -6,12 +6,13 @@ Python heapq is minHeap
 Java priority Queue is minHeap
 *(-1) to make maxHeap
 
-heapq.heappush(heap, item)
-heapq.heappop(heap)
-heapq.heappushpop(heap, item) is more efficient than (heappush + heappop)
+heapq.heappush(list, item)
+heapq.heappop(list)
+heapq.heappushpop(list, item) is more efficient than (heappush + heappop)
 heapq.heapify(x)
 heapq.nlargest(n, iterable, key=None)
 heapq.nsmallest(n, iterable, key=None)
+list[0] -> peak
 '''
 
 
@@ -23,6 +24,6 @@ heapq.heappush(a, 4)
 # Time O(logN)
 heapq.heappop(a)
 # Time O(NlogK)
-b_dict = collections.Counter(a)
-heapq.nlargest(3, b_dict, key=b.dict.get)
-heapq.nsmallest(3, b_dict, key=b_dict.get)
+# b_dict = collections.Counter(a)
+# heapq.nlargest(3, b_dict, key=b.dict.get)
+# heapq.nsmallest(3, b_dict, key=b_dict.get)
